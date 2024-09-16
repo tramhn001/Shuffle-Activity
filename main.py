8c7a76a9d34d7572fa513f1eb80c1b5d992dd9ec
Ashley Yang <ashley.minghui@gmail.com>
2024-09-16 09:50:07 -0700

    Port Shuffle Activity

---
+++ main.py
@@ -0,0 +1,9 @@
+CARDS = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+SUITS = ["♥", "♦", "♠", "♦︎"]
+
+deck_of_cards = [f"{card}{suit}" for card in CARDS for suit in SUITS]
+
+# Implement a solution for shuffling a deck of cards without using random.shuffle()
+# Using randint() is ok
+def shuffle_deck(deck_of_cards):
+    pass
