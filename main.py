3cf6ecd55e060a6ffe48ddac5fcd08794a1c21c9
Ashley Yang <ashley@adadevelopersacademy.org>
2024-09-16 14:58:09 -0700

    Update main.py to remove double diamonds

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 CARDS = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
-SUITS = ["♥", "♦", "♠", "♦︎"]
+SUITS = ["♥", "♦", "♠", "♣"]
 
 deck_of_cards = [f"{card}{suit}" for card in CARDS for suit in SUITS]
 
