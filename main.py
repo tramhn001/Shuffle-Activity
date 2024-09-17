a4cb33534050899b2407daab5d9e103bc7274045
Tram <tramnhoang@gmail.com>
2024-09-16 22:24:34 -0700

    Shuffled activities

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 CARDS = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 SUITS = ["♥", "♦", "♠", "♣"]
 
@@ -6,4 +8,18 @@ deck_of_cards = [f"{card}{suit}" for card in CARDS for suit in SUITS]
 # Implement a solution for shuffling a deck of cards without using random.shuffle()
 # Using randint() is ok
 def shuffle_deck(deck_of_cards):
-    pass
+
+    shuffled_deck = []
+    indices_used = set()
+
+    while len(shuffled_deck) < len(deck_of_cards):
+        index = random.randint(0, len(deck_of_cards) - 1)
+        if index not in indices_used:
+            shuffled_deck.append(deck_of_cards[index])
+            indices_used.add(index)
+    return shuffled_deck
+
+
+shuffled = shuffle_deck(deck_of_cards)
+print("Shuffled deck:", shuffled)
+
